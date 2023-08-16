4fc43909755e2a9f1c7d88ca207fffaf1b2e5ad4
stephen <stephen.ma@xcreators.com>
2023-08-16 23:29:48 +1000

    BUILD: config.

---
+++ internal/config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"github.com/google/uuid"
+)
+
+var Config MessagingEngineConfig
+
+var EngineId string
+
+type AuthConfig struct {
+	AuthMiddlewareSecretKey string `json:"auth_middleware_secret_key"`
+	JwtCookieName           string `json:"jwt_cookie_name"`
+	CsrfCookieName          string `json:"csrf_cookie_name"`
+	CsrfHeaderName          string `json:"csrf_header_name"`
+}
+
+type MessagingEngineConfig struct {
+	SvcHost        string     `json:"svc_host"`
+	SvcPort        int        `json:"svc_port"`
+	AllowedOrigins string     `json:"allowed_origins"` // comma seperated origins
+	Auth           AuthConfig `json:"auth"`
+}
+
+func init() {
+	// claim my id
+	EngineId = uuid.New().String()
+}
